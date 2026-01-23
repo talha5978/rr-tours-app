d60b243922c40f329a5cbcdd60729befbb570aac
Muhammad Talha <muhammadtalha13457@gmail.com>
2026-01-23 16:27:53 +0500

    Add facebook link

--- shared/src/constants/constants.ts
+++ shared/src/constants/constants.ts
@@ -2,7 +2,7 @@ import { Constants } from "@workspace/shared/types/supabase";
 
 export const CONTACT_NUMBER_1 = "971556130581" as const;
 export const INSTAGRAM_URL = "https://www.instagram.com/top_attractionsdubai" as const;
-export const FB_URL = "https://www.instagram.com/top_attractionsdubai" as const;
+export const FB_URL = "https://www.facebook.com/share/g/1AqEVNTwYG/?mibextid=wwXIfr" as const;
 
 export const STORAGE_BUCKETS = {
 	images: "images",
