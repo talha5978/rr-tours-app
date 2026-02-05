de25e3b9bba3bf6b007a3217ec51dcb120dd3613
Muhammad Talha <muhammadtalha13457@gmail.com>
2026-02-05 21:44:25 +0500

    Add static routes config

--- front-panel/react-router.config.ts
+++ front-panel/react-router.config.ts
@@ -4,4 +4,7 @@ import { vercelPreset } from "@vercel/react-router/vite";
 export default {
 	ssr: true,
 	presets: [vercelPreset()],
+	async prerender() {
+		return ["/faqs", "privacy-policy", "/terms-of-usage", "about"];
+	},
 } satisfies Config;
